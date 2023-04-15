3c4a76349e434b10150fb1d3863b4b0361ce86af
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2023-04-15 17:35:20 +0300

    my rigidbody basic script

---
+++ Assets/Scripts/Phyisics/MyRigidBody2D.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyRigidBody2D : MonoBehaviour
+{
+
+    public Vector2 acceleration;
+    public Vector2 Force;
+    public Vector2 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
