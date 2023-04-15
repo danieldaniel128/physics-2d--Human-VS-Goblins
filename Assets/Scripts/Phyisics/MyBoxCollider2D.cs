d6feb5f4d2c379d97ef369ce6b7338952d38b426
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2023-04-15 17:34:57 +0300

    regions and collider with rotation

--- Assets/Scripts/Phyisics/MyBoxCollider2D.cs
+++ Assets/Scripts/Phyisics/MyBoxCollider2D.cs
@@ -1,6 +1,5 @@
+using System;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
-
 public class MyBoxCollider2D : MonoBehaviour
 {
     public float Width;
@@ -8,19 +7,20 @@ public class MyBoxCollider2D : MonoBehaviour
     public float WidthOffSet;
     public float HeightOffSet;
     public float Mass;
-
-    private void Start()
-    {
-    }
+    public float RotationAngle;
+    public event Action<MyBoxCollider2D> OnCollision;
 
     //adding מקדם חיכוך
+
+    private Quaternion _eulerAngleEdges => Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z);
+
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawRay(transform.position + (Vector3.left * Width + Vector3.down * Height) / 2f, Vector2.right * Width);
-        Gizmos.DrawRay(transform.position + (Vector3.left * Width + Vector3.up * Height) / 2f, Vector2.right * Width);
-        Gizmos.DrawRay(transform.position + (Vector3.left * Width + Vector3.down * Height) / 2f, Vector2.up * Height);
-        Gizmos.DrawRay(transform.position + (Vector3.right * Width + Vector3.down * Height) / 2f, Vector2.up * Height);
+        Gizmos.DrawRay(transform.position + _eulerAngleEdges * ((Vector3.left * Width + Vector3.down * Height) / 2f), _eulerAngleEdges * Vector2.right * Width);
+        Gizmos.DrawRay(transform.position + _eulerAngleEdges * (Vector3.left * Width + Vector3.up * Height) / 2f, _eulerAngleEdges * Vector2.right * Width);
+        Gizmos.DrawRay(transform.position + _eulerAngleEdges * (Vector3.left * Width + Vector3.down * Height) / 2f, _eulerAngleEdges * Vector2.up * Height);
+        Gizmos.DrawRay(transform.position + _eulerAngleEdges * (Vector3.right * Width + Vector3.down * Height) / 2f, _eulerAngleEdges * Vector2.up * Height);
     }
 }
--- Assets/Scripts/Phyisics/Physics2DManager.cs
+++ Assets/Scripts/Phyisics/Physics2DManager.cs
@@ -1,17 +1,35 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using UnityEditor;
+using System.Linq;
 using UnityEngine;
 
-public static class Physics2DManager
+public class Physics2DManager
 {
     //after, i will do a collider script that different kinds of colliders will inherit from and the array will be of collider
-    static List<MyBoxCollider2D> _myBoxColliders2D = new List<MyBoxCollider2D>();
+    static List<MyBoxCollider2D> _myBoxColliders2D = new List<MyBoxCollider2D>();//collider get added with the events.
     public static event Action<MyBoxCollider2D> ColliderWasAdded;
     public static event Action<MyBoxCollider2D> ColliderWasRemoved;
 
+    public static float DeltaTime;
+
+
+
+
+
+    #region Colliding Logic 
+    //OnCollision?
+    public void CollidersAreTouching() 
+    {
+        //_myBoxColliders2D.Where(a => _myBoxColliders2D.Where(b =>b!=a && Vector3.Distance(a.transform.position,b.transform.position) < )  )
+        
+    
+    }
+    #endregion
+
+
+
+    #region ColliderWasAdded Subsribtion 
     public static void InvokeColliderWasAdded(MyBoxCollider2D colliderWasAdded)
     {
         ColliderWasAdded?.Invoke(colliderWasAdded);
@@ -27,7 +45,6 @@ public static class Physics2DManager
         foreach (var adsdas in _myBoxColliders2D)
             Debug.Log(adsdas);
     }
-
     public static void OnAddToCollider() 
     {
         ColliderWasAdded += AddToColliderList;
@@ -45,7 +62,7 @@ public static class Physics2DManager
     {
         _myBoxColliders2D.Remove(collider);
     }
-
+    #endregion
 
 
 
