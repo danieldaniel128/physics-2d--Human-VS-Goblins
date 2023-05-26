72a9ff9bacb63323b1736f7843e915b93c4a5c61
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2023-05-27 02:39:01 +0300

    canvas screen overlay to screen camera(better ui) converting

--- Assets/Scripts/UI/HealthBarFollow.cs
+++ Assets/Scripts/UI/HealthBarFollow.cs
@@ -10,11 +10,21 @@ public class HealthBarFollow : MonoBehaviour
     public Image HealthBarImage;
     public float yOffset;
     public float xOffset;
+    Canvas healthBarCanvas;
 
+    private void Start()
+    {
+        healthBarCanvas = transform.parent.GetComponentInParent<Canvas>();
+    }
     private void LateUpdate()
     {
         // Update the position of the health bar to match the enemy's position
-        healthBarRectTransform.position = Camera.main.WorldToScreenPoint(TargetTransform.position + Vector3.right * xOffset + Vector3.up * yOffset);
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(TargetTransform.position + Vector3.right * xOffset + Vector3.up * yOffset);
+        //healthBarRectTransform.position = Camera.main.WorldToViewportPoint(TargetTransform.position + Vector3.right * xOffset + Vector3.up * yOffset);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(healthBarCanvas.transform as RectTransform, screenPosition, healthBarCanvas.worldCamera, out Vector2 canvasPosition);
 
+
+        // Set the canvas position of the health bar
+        healthBarRectTransform.localPosition = canvasPosition;
     }
 }
