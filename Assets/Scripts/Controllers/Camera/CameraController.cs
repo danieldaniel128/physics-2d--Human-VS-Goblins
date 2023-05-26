a09b4057656ba029b3f24cb72bc99354e7f97a2b
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2023-05-27 01:17:52 +0300

    camera moving only x axis, level design enemy spawner

--- Assets/Scripts/Controllers/Camera/CameraController.cs
+++ Assets/Scripts/Controllers/Camera/CameraController.cs
@@ -10,11 +10,11 @@ public class CameraController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D))
             transform.position += Vector3.right * _cameraSpeedX * Time.deltaTime;
-        else if (Input.GetKey(KeyCode.A))
+       else if (Input.GetKey(KeyCode.A))
             transform.position += Vector3.left * _cameraSpeedX * Time.deltaTime;
-        if (Input.GetKey(KeyCode.W))
-            transform.position += Vector3.up * _cameraSpeedY * Time.deltaTime;
-        else if (Input.GetKey(KeyCode.S))
-            transform.position += Vector3.down * _cameraSpeedY * Time.deltaTime;
+        //if (Input.GetKey(KeyCode.W))
+        //    transform.position += Vector3.up * _cameraSpeedY * Time.deltaTime;
+        //else if (Input.GetKey(KeyCode.S))
+        //    transform.position += Vector3.down * _cameraSpeedY * Time.deltaTime;
     }
 }
