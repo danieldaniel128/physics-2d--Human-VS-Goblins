dc22021535426c02cccabc4b42ce3f3f4700a678
danieldaniel128 <107756664+danieldaniel128@users.noreply.github.com>
2023-05-29 12:34:33 +0300

    miner going to mine and castle

--- Assets/Scripts/Controllers/MinerController.cs
+++ Assets/Scripts/Controllers/MinerController.cs
@@ -1,24 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class MinerController : MonoBehaviour
 {
     [SerializeField] MyBoxCollider2D minerCollider;
+    [SerializeField] MyRigidBody2D minerRigidBody;
+    
+    [SerializeField] float _minerSpeed;
+    public float MinerSpeed { get { return _minerSpeed; } private set { _minerSpeed = value; } }
 
     private void Start()
     {
         minerCollider.OnCollisionEnter += OnMyCollisionEnter2D;
-        //minerCollider.OnCollisionExit += OnMyCollisionExit2D;
+        minerCollider.OnCollisionExit += OnMyCollisionExit2D;
     }
-    //void OnMyCollisionExit2D(MyBoxCollider2D collider)
-    //{
 
-    //}
+
+    private void FixedUpdate()
+    {
+        MoveTowardsCastleAndMine();
+    }
+
+        void OnMyCollisionExit2D(MyBoxCollider2D collider)
+        {
+            if (collider.tag.Equals("Castle"))
+            {
+                MinerSpeed *= -1;
+                transform.position += Vector3.left * 0.05f;
+            }
+            else if (collider.tag.Equals("Mine"))
+            {
+                MinerSpeed *= -1;
+                transform.position += Vector3.right * 0.05f;
+            }
+
+        //enemyCollider.OnCollisionEnter
+    }
+
+    void MoveTowardsCastleAndMine()
+    {
+        minerRigidBody.velocity = new Vector2(_minerSpeed, minerRigidBody.velocity.y); //speed directed to castle, or mone carring the resources
+    }
     void OnMyCollisionEnter2D(MyBoxCollider2D collider)
     {
         minerCollider.SetOnCollisionWith(CanCollideWithCollider);
+        
     }
     bool CanCollideWithCollider(MyBoxCollider2D collider)//true is cant, false is not
     {
